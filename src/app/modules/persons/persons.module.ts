cee5740db45477e6af11f5e600f28e6a0692a2f5
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-10-20 13:45:28 +0200

    cleaning

--- src/app/modules/persons/persons.module.ts
+++ src/app/modules/persons/persons.module.ts
@@ -1,26 +1,12 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PersonsComponent} from './persons.component';
 import {MatCardModule, MatIconModule, MatListModule} from '@angular/material';
 import {PersonService} from './services/person.service';
 import {PersonEditComponent} from './person-edit/person-edit.component';
 import {RouterModule, Routes} from '@angular/router';
-import {NgxsModule, Store} from '@ngxs/store';
-import {Person} from './models/person.model';
-import {InitMainState} from './store/actions/main-state.actions';
+import {NgxsModule} from '@ngxs/store';
 import {PersonsState} from './store/states/persons-state.state';
-
-export function loadPerson(personService: PersonService, store: Store) {
-  return () => {
-    console.log('Load Persons at Start');
-    const persons: Person[] = [];
-    personService.getPersons().subscribe(
-      (person) => persons.push(person),
-      (error) => console.log(error),
-      () => store.dispatch(new InitMainState(persons))
-    );
-  };
-}
 
 const personRoutes: Routes = [
   {
@@ -50,7 +36,6 @@ const personRoutes: Routes = [
   ],
   declarations: [PersonsComponent, PersonEditComponent],
   providers: [
-    {provide: APP_INITIALIZER, useFactory: loadPerson, deps: [PersonService, Store], multi: true},
     PersonService
   ]
 })
