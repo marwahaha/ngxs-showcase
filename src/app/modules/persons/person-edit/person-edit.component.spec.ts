bc88a513704f00ee13563dee81c3a4d59cc426f6
Stéphane Krattinger <stef.krattinger@gmail.com>
2018-10-22 07:13:10 +0200

    fix test person edit component

--- src/app/modules/persons/person-edit/person-edit.component.spec.ts
+++ src/app/modules/persons/person-edit/person-edit.component.spec.ts
@@ -1,12 +1,25 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PersonEditComponent} from './person-edit.component';
+import {MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('PersonEditComponent', () => {
   let component: PersonEditComponent;
   let fixture: ComponentFixture<PersonEditComponent>;
 
   setupTestBed({
+    imports: [
+      ReactiveFormsModule,
+      MatInputModule,
+      MatButtonModule,
+      MatDatepickerModule,
+      MatFormFieldModule,
+      MatMomentDateModule,
+      NoopAnimationsModule
+    ],
     declarations: [PersonEditComponent]
   });
 
@@ -20,7 +33,8 @@ describe('PersonEditComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and be invalid', () => {
     expect(component).toBeTruthy();
+    expect(component.personForm.valid).toBe(false);
   });
 });
